ada502781d49bce0ff2342a2bbdde0caa638f301
jdrockmael <jdrockmael@wpi.edu>
2022-03-21 21:25:45 -0400

    Jason changed name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Andres Negron");
+    System.out.println("Jason Rockmael");
     App.launch(App.class, args);
   }
 }
