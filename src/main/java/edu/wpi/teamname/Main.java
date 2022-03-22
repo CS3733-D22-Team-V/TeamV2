53052a74675fa647ae5cbcd47345065cd6728819
mchrpt <mchrpt@yahoo.com>
2022-03-21 20:33:47 -0400

    Added name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,8 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Bazinga");
-    System.out.println("Tucker Raymond");
+    System.out.println("Matthew Hendrickson");
     App.launch(App.class, args);
   }
 }
