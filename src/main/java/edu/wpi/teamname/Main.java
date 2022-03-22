05af213b89a9796864dc9016d93add6fd4080f42
Tucker Raymond <teraymond@wpi.edu>
2022-03-21 20:36:28 -0400

    Added name (again)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Tucker Raymond");
+    System.out.print("create");
     App.launch(App.class, args);
   }
 }
