3b54c78802363fbd4b641be06d31f49fb7d4dacd
Jolene Pern <jpern@wpi.edu>
2022-03-21 21:39:04 -0400

    added my name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Ben Bagley");
+    System.out.println("Jolene Pern");
     App.launch(App.class, args);
   }
 }
