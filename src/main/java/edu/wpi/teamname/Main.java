48fd9e96e190f0bef5c19ffb70f82344a1d8d24e
Tucker Raymond <teraymond@wpi.edu>
2022-03-21 20:38:19 -0400

    Added name (again)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("BMERMERMEMRERMERMEGE CONFLICT");
+    System.out.println("NO MORE BAZINGA CONFLICT");
     App.launch(App.class, args);
   }
 }
