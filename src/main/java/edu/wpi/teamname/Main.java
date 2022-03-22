db355a5806d389b546989c983975ac72f1dc3b16
MottledPetrel <vzmemez@gmail.com>
2022-03-21 20:57:06 -0400

    Dylan

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("NO MORE BAZINGA CONFLICT");
+    System.out.println("Dylan Hunt");
     App.launch(App.class, args);
   }
 }
